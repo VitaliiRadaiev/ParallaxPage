a9aa687760b4168286ad2643019b0aad4a34efd6
VitaliiRadaiev <hf.chelenger@gmail.com>
2020-07-07 12:21:30 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,32 @@
+
+
+$(document).ready(function() {
+
+// === Проверка, поддержка браузером формата webp ==================================================================
+
+
+	function testWebP(callback) {
+
+	var webP = new Image();
+	webP.onload = webP.onerror = function () {
+	callback(webP.height == 2);
+	};
+	webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+	}
+
+	testWebP(function (support) {
+
+	if (support == true) {
+	document.querySelector('body').classList.add('webp');
+	}else{
+	document.querySelector('body').classList.add('no-webp');
+	}
+	});
+
+// === // Проверка, поддержка браузером формата webp ==================================================================
+$(document).ready(function() {
+	$('.parallax__list>li').addClass('layer');
+	$('.parallax__list').parallax();
+	$('.wrapper').addClass('active');
+})
+});
